ee5011c577ddc60001137a12f0a1a28d3d993c24
Turkheim <31385569+Turkheim@users.noreply.github.com>
2025-07-26 19:09:02 -0300

    Added New Levels, New floor texture

--- Scripts/WinMenu.gd
+++ Scripts/WinMenu.gd
@@ -21,9 +21,13 @@ func _on_next_level_button_pressed() -> void:
 	_unpause()
 	if level == 1:
 		get_tree().change_scene_to_file("res://Scenes/Levels/level_02.tscn")
-	if level == 2:
+	elif level == 2:
 		get_tree().change_scene_to_file("res://Scenes/Levels/level_03.tscn")
-	if level == 3:
+	elif level == 3:
 		get_tree().change_scene_to_file("res://Scenes/Levels/level_04.tscn")
-	if level == 4:
+	elif level == 4:
+			get_tree().change_scene_to_file("res://Scenes/Levels/level_05.tscn")
+	elif level == 5:
+		get_tree().change_scene_to_file("res://Scenes/Levels/level_06.tscn")
+	elif level == 6:
 		get_tree().change_scene_to_file("res://Scenes/StartMenu.tscn")
